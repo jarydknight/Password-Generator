3416f3a18666b8f456b9d70848f46e22a9ca1d3f
Jaryd Knight <jaryd.knight@hotmail.com>
2022-05-22 15:34:26 -0400

    add user prompts for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,16 @@
 // Assignment code here
 
+const passwordCriteriaPrompt = () => {
+  const passwordLength = window.prompt("Enter the length for your password. The length must be between 8 and 128 characters.");
+  const upperCaseChar = window.confirm("Do you want to include upper case letters in yoru password?");
+  const lowerCaseChar = window.confirm("Do you want to include lower case letters in your password?")
+  const numChar = window.confirm("Do you want to include numbers in your password?");
+  const specialChar = window.confirm("Do you want to include special characters in your password?");
+}
+
+const generatePassword = () => {
+  passwordCriteriaPrompt();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
